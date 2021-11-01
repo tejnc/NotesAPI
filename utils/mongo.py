cc65b14f769166a17909de6d68b7b4a09053680a
tejnc <tejnarayan15.tnc@gmail.com>
2021-11-01 16:16:15 +0545

    Added utilities

---
+++ utils/mongo.py
@@ -0,0 +1,18 @@
+import os 
+
+from mongoengine import connect
+
+
+def get_db_connection():
+    url = "mongodb+srv://test:{}@cluster0.x25kn.mongodb.net/{}?retryWrites=true&w=majority".format(
+        os.environ["DB_PASS"], os.environ["DB"]
+    )
+    return url
+   
+    
+def db_config():
+    try:
+        uri: str = get_db_connection()
+        connect(host=uri)
+    except KeyError:
+        connect("mongoenginetest",host="mongomock://localhost")
